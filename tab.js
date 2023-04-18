e80d5ad86d8be4c500535565753567fa07cda073
Emerson14machado <115734319+Emerson14machado@users.noreply.github.com>
2023-04-18 13:41:36 -0300

    Add files via upload

---
+++ tab.js
@@ -0,0 +1,24 @@
+const toggleBtn = document.getElementById('toggle-mode');
+    const body = document.body;
+
+    toggleBtn.addEventListener('click', function () {
+        body.classList.toggle('dark-mode')
+    })
+
+function gerarTabuada() {
+    var numero = parseInt(document.getElementById('numero').value); //Gera numeros inteiros sem virgula.
+    var tabela = document.getElementById('tabela-tabuada');
+
+    tabela.innerHTML = " "; //Limpar a tabela antes de gerar outra.
+    ////////////////
+    ///////////////
+    
+    //Gerar tabuada.
+
+    for (var i = 1; i <= 10; i++) {
+        var resultado = numero * i;
+        var linha = "<tr><td>" + numero + " X " + i + "</td> <td>" + resultado + "</td></tr>";
+        tabela.innerHTML += linha
+    }
+
+}
